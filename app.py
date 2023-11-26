410c609b9944f3a510f96e58916ce822f3c33ea7
Yesh-adithya31 <57474408+Yesh-adithya31@users.noreply.github.com>
2023-11-26 23:47:34 +0530

    7 commit

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 # Initialize Firebase Admin SDK
 cred = credentials.Certificate('serviceAccountKey.json')
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://myapp-a0cd7-default-rtdb.firebaseio.com/'
+    'databaseURL': 'https://myapp-a0cd7-default-rtdb.firebaseio.com/',
+    'storageBucket': 'gs://myapp-a0cd7.appspot.com'
 })
 
 # User Registration Endpoint
@@ -53,11 +54,28 @@ def login_user():
 @app.route('/store_data', methods=['POST'])
 def store_user_data():
     data = request.get_json()
-    user_id = data['user_id']
+    email = data['email']
+    user = auth.get_user_by_email(email)
     # Assuming 'users' is a collection in Firebase Realtime Database
-    ref = db.reference('users/' + user_id)
-    ref.set(data)  # Store user data
-    return jsonify({"message": "User data stored successfully"})
+    ref = db.reference('users/' + user.uid)
+    ref.update(data)  # Store user data
+    return jsonify({"message": "User data updated successfully"})
+
+
+@app.route('/upload', methods=['POST'])
+def upload_data():
+    try:
+        data = request.get_json()
+        email = data['email']
+
+        user = auth.get_user_by_email(email)
+        ref = db.reference('emergency/' + user.uid)
+        ref.set(data)
+        return jsonify({"message": "Media uploaded successfully"})
+
+    except Exception as e:
+        return jsonify({"message": "Upload failed", "error": str(e)}), 400
+
 
 if __name__ == '__main__':
     app.run(debug=True)
